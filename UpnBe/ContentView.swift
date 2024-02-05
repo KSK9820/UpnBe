66e093b0a093b7c99a9243e766d01acf01aaadac
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-02-05 11:47:51 +0900

    [Config] 초기 설정

---
+++ UpnBe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UpnBe
+//
+//  Created by 김수경 on 2024/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UpnBe/UpnBeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpnBeApp.swift
+//  UpnBe
+//
+//  Created by 김수경 on 2024/02/05.
+//
+
+import SwiftUI
+
+@main
+struct UpnBeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
