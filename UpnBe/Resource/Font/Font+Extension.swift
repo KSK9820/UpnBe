cd7d63f7edba6a54d62d7e057ba5a0aa5cfeaaae
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-02-05 11:50:12 +0900

    [Config] Custom Font, Color 추가

---
+++ UpnBe/Resource/Font/Font+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  Font+Extension.swift
+//  UpnBe
+//
+//  Created by 김수경 on 2024/02/05.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // MARK: - Extra Bold
+    
+    static let heading1 = Font.custom("NotoSans-ExtraBold", size: 24)
+    static let heading2 = Font.custom("NotoSans-ExtraBold", size: 20)
+    static let heading3 = Font.custom("NotoSans-ExtraBold", size: 18)
+    static let heading4 = Font.custom("NotoSans-ExtraBold", size: 16)
+    static let heading5 = Font.custom("NotoSans-ExtraBold", size: 14)
+    static let heading6 = Font.custom("NotoSans-ExtraBold", size: 12)
+    
+    // MARK: - Medium
+    
+    static let paragraph1 = Font.custom("NotoSans-Medium", size: 20)
+    static let paragraph2 = Font.custom("NotoSans-Medium", size: 18)
+    static let paragraph3 = Font.custom("NotoSans-Medium", size: 16)
+    static let paragraph4 = Font.custom("NotoSans-Medium", size: 14)
+    static let paragraph5 = Font.custom("NotoSans-Medium", size: 12)
+}
