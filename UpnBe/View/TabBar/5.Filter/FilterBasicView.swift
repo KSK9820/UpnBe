baa22f1fd9b57eaf7503da1d3991d096126075d2
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-02-08 16:08:43 +0900

    [Refactor] FilteredBasicView의 Constraint 수정 및 재설정 버튼 추가

--- UpnBe/View/TabBar/5.Filter/FilterBasicView.swift
+++ UpnBe/View/TabBar/5.Filter/FilterBasicView.swift
@@ -16,37 +16,55 @@ enum Filter: Int {
 
 final class FilterBasicViewModel: ObservableObject {
     @Published var filterStatus: [Filter: [Bool]] = [
-            .division: [false, false, false, false, false],
-            .drill: [false, false, false, false, false],
-            .location: [false, false, false, false],
-            .party: [false, false, false]
-        ]
+        .division: [false, false, false, false, false],
+        .drill: [false, false, false, false, false],
+        .location: [false, false, false, false],
+        .party: [false, false, false]
+    ]
     
     func changeStatus(classify: Filter, index: Int) {
-            filterStatus[classify] = filterStatus[classify]?.map { _ in false }
-            filterStatus[classify]?[index] = true
-        }
+        filterStatus[classify] = filterStatus[classify]?.map { _ in false }
+        filterStatus[classify]?[index] = true
+    }
 }
 
 struct FilterBasicView: View {
     @ObservedObject var viewModel = FilterBasicViewModel()
     
     let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
-    let filterContent: [Filter: [String]] = [
-        .division: ["뉴비", "루키", "아마추어", "올스타", "세미프로"],
-        .drill: ["볼마스터리", "드리블", "슈팅", "패스", "트레이닝"],
-        .location: ["집", "미니풋살장", "공터", "정규풋살장"],
-        .party: ["혼자서", "둘이서", "여러명이서"]
-    ]
-
+    
+    
     var body: some View {
         ZStack {
             Color.black
-            
             VStack {
+                HStack {
+                    Spacer()
+                    
+                    Button() {
+                        viewModel.filterStatus = [
+                            .division: [false, false, false, false, false],
+                            .drill: [false, false, false, false, false],
+                            .location: [false, false, false, false],
+                            .party: [false, false, false]
+                        ]
+                    } label: {
+                        if containsTrue() {
+                            Text("재설정")
+                                .underline()
+                                .foregroundStyle(.background4)
+                        } else {
+                            Text(" ")
+                        }
+                    }
+                    .horizontalPadding(direction: .trailing, size: 10)
+                    .verticalPadding(direction: .top, size: 10)
+                }
+                
                 Text("디비전")
                     .font(.heading4)
                     .foregroundStyle(.white)
+                    .verticalPadding(direction: .top, size: 11)
                 configureSection(.division, count: 3)
                 
                 Text("드릴")
@@ -54,11 +72,12 @@ struct FilterBasicView: View {
                     .foregroundStyle(.white)
                 configureSection(.drill, count: 3)
                 
+                
                 Text("장소")
                     .font(.heading4)
                     .foregroundStyle(.white)
                 configureSection(.location, count: 2)
-
+                
                 Text("참여인원")
                     .font(.heading4)
                     .foregroundStyle(.white)
@@ -67,12 +86,11 @@ struct FilterBasicView: View {
                 Text("적용하기")
                     .applyMintButtonModifier(width: 320, height: 50)
                     .verticalPadding(direction: .top, size: 5)
-               
+                    .verticalPadding(direction: .bottom, size: 22)
             }
             .background {
                 RoundedRectangle(cornerRadius: 20)
                     .fill(Color(.background1))
-                    .frameForUPNBE(width: 340, height: 600)
             }
             .horizontalPadding(direction: .horizontal, size: 10)
             .verticalPadding(direction: .bottom, size: 10)
@@ -85,6 +103,12 @@ struct FilterBasicView: View {
 extension FilterBasicView {
     @ViewBuilder
     private func configureSection(_ filter: Filter, count: Int) -> some View {
+        let filterContent: [Filter: [String]] = [
+            .division: ["뉴비", "루키", "아마추어", "올스타", "세미프로"],
+            .drill: ["볼마스터리", "드리블", "슈팅", "패스", "트레이닝"],
+            .location: ["집", "미니풋살장", "공터", "정규풋살장"],
+            .party: ["혼자서", "둘이서", "여러명이서"]
+        ]
         let contentCount = filterContent[filter]?.count ?? 0
         let rowCount = contentCount / count
         
@@ -92,7 +116,7 @@ extension FilterBasicView {
             ForEach(0...rowCount, id: \.self) { num in
                 let startIndex = num * count
                 let endIndex = startIndex + count > contentCount ? contentCount : startIndex + count
-                HStack {
+                HStack(spacing: 10) {
                     ForEach(startIndex..<endIndex, id: \.self) { colIndex in
                         Button {
                             viewModel.changeStatus(classify: filter, index: colIndex)
@@ -107,8 +131,17 @@ extension FilterBasicView {
                 }
             }
         }
-        .verticalPadding(direction: .top, size: 15)
         .verticalPadding(direction: .bottom, size: 20)
+        .horizontalPadding(direction: .horizontal, size: 10)
+    }
+    
+    private func containsTrue() -> Bool {
+        for (_, status) in viewModel.filterStatus {
+            if status.contains(true) {
+                return true
+            }
+        }
+        return false
     }
 }
 
